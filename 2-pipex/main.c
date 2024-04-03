f1c93ad68266e25377103b80adbd1c495f4fa790
lagilogi <wouter_s@hotmail.nl>
2024-04-03 18:35:20 +0200

    Added some tryout code and removed all .o files

--- 2-pipex/main.c
+++ 2-pipex/main.c
@@ -22,106 +22,184 @@ static void	checking_parsing(t_info *info) // CHECKING PARSING
 }
 
 
-int	main(int argc, char **argv, char **envp)
-{
-	t_info	info;
-
-	// 1. Checking argument amount
-	if (argc < 5)
-		return (1);
-
-	// 2. Opening files
-	info.fd_0 = open(argv[1], O_RDONLY); // IN or OUTPUT?
-	info.fd_1 = open(argv[4], O_RDONLY); // IN or OUTPUT?
-
-	// 3. Parsing commands
-	parsing_command(&info, argc, argv);
-	checking_parsing(&info);
-
-
-
-	char *str[] = {"ls", NULL}; // TESTING EXECVE FUNCTION
-	execve("/bin/ls", str, envp); // TESTING EXECVE FUNCTION
-
-
-
-	// 4. Creating pipe
-
-	// 5. Forking
-
-	// 6. Executing command 1 on file 1
-
-	// 7. Piping
-
-	// 9. Executing command 2 on that created information
-
-	// 10. Writing that information into file 2
-
-	// 11. Error handling
-
-	kill_program(&info, "Ended program succesfully!", 1);
-	return (0);
-}
-
-
-// int		main(int argc, char **argv)
+// int	main(int argc, char **argv, char **envp)
 // {
-// 	// int	id = fork(); // Forking basically creates a duplicate of a program and runs both the "main" program and the "duplicate" program at the same time
-// 	// if (id == 0) // Child process always has process ID 0
-// 	// 	printf("This is the child and has id %d\n", id);
-// 	// else		// Parent process is never ID 0
-// 	// 	printf("This is the parent with id %d\n", id);
+// 	t_info	info;
 
-// 	// int fd[2]; // TRYOUT 2
-// 	// if (pipe(fd) == -1)
-// 	// {
-// 	// 	printf("Error occured with opening Pipe\n");
-// 	// 	return (1);
-// 	// }
-// 	// int id = fork();
-// 	// if (id == 0)
-// 	// {
-// 	// 	close(fd[0]);
-// 	// 	int x;
-// 	// 	printf("input a number: ");
-// 	// 	scanf("%d", &x);
-// 	// 	if (write(fd[1], &x, sizeof(int)) == -1)
-// 	// 		return (1);
-// 	// 	close(fd[1]);
-// 	// }
-// 	// else
-// 	// {
-// 	// 	close(fd[1]);
-// 	// 	int y;
-// 	// 	if (read(fd[0], &y, sizeof(int)) == -1)
-// 	// 		return (1);
-// 	// 	close(fd[0]);
-// 	// 	printf("\nGot from child process %d\n\n", y);
-// 	// 	wait(NULL);
-// 	// }
+// 	// 1. Checking argument amount
+// 	if (argc < 5)
+// 		return (1);
 
-// 	// int fd[2]; // TRYOUT 3
-// 	// pipe(fd);
-// 	// int pid = fork();
-// 	// if (pid == -1)
-// 	// 	return (1);
+// 	// 2. Opening files
+// 	info.fd_0 = open(argv[1], O_RDONLY); // IN or OUTPUT?
+// 	info.fd_1 = open(argv[4], O_RDONLY); // IN or OUTPUT?
 
-// 	// if (pid == 0) // Child process
-// 	// {
-// 	// 	close(fd[0]);
-// 	// 	int x = 5;
-// 	// 	if (write(fd[1], &x, sizeof(int)) == -1)
-// 	// 		return (1);
-// 	// 	close(fd[1]);
-// 	// }
-// 	// else
-// 	// {
-// 	// 	close(fd[1]);
-// 	// 	int y;
-// 	// 	if (read(fd[0], &y, sizeof(int)) == -1)
-// 	// 		return (1);
-// 	// 	printf("Got nmbr: %d\n", y);
-// 	// 	close(fd[0]);
-// 	// }
+// 	// 3. Parsing commands
+// 	parsing_command(&info, argc, argv);
+// 	checking_parsing(&info);
+
+
+
+// 	char *str[] = {"ls", NULL}; // TESTING EXECVE FUNCTION
+// 	execve("/bin/ls", str, envp); // TESTING EXECVE FUNCTION
+
+
+
+// 	// 4. Creating pipe
+
+// 	// 5. Forking
+
+// 	// 6. Executing command 1 on file 1
+
+// 	// 7. Piping
+
+// 	// 9. Executing command 2 on that created information
+
+// 	// 10. Writing that information into file 2
+
+// 	// 11. Error handling
+
+// 	kill_program(&info, "Ended program succesfully!", 1);
+// 	return (0);
 // }
+
+
+int		main(int argc, char **argv)
+{
+	// int	id = fork(); // Forking basically creates a duplicate of a program and runs both the "main" program and the "duplicate" program at the same time
+	// if (id == 0) // Child process always has process ID 0
+	// 	printf("This is the child and has id %d\n", id);
+	// else		// Parent process is never ID 0
+	// 	printf("This is the parent with id %d\n", id);
+
+
+
+	// int fd[2]; // TRYOUT 2
+	// if (pipe(fd) == -1)
+	// {
+	// 	printf("Error occured with opening Pipe\n");
+	// 	return (1);
+	// }
+	// int id = fork();
+	// if (id == 0)
+	// {
+	// 	close(fd[0]);
+	// 	int x;
+	// 	printf("input a number: ");
+	// 	scanf("%d", &x);
+	// 	if (write(fd[1], &x, sizeof(int)) == -1)
+	// 		return (1);
+	// 	close(fd[1]);
+	// }
+	// else
+	// {
+	// 	close(fd[1]);
+	// 	int y;
+	// 	if (read(fd[0], &y, sizeof(int)) == -1)
+	// 		return (1);
+	// 	close(fd[0]);
+	// 	printf("\nGot from child process %d\n\n", y);
+	// 	wait(NULL);
+	// }
+
+
+
+	// int fd[2]; // TRYOUT 3
+	// pipe(fd);
+	// int pid = fork();
+	// if (pid == -1)
+	// 	return (1);
+	// if (pid == 0) // Child process
+	// {
+	// 	close(fd[0]);
+	// 	int x = 5;
+	// 	if (write(fd[1], &x, sizeof(int)) == -1)
+	// 		return (1);
+	// 	close(fd[1]);
+	// }
+	// else
+	// {
+	// 	close(fd[1]);
+	// 	int y;
+	// 	if (read(fd[0], &y, sizeof(int)) == -1)
+	// 		return (1);
+	// 	printf("Got nmbr: %d\n", y);
+	// 	close(fd[0]);
+	// }
+
+
+
+	// int fd[2]; // TRYOUT 4
+	// pipe(fd);
+	// int pid = fork();
+	// if (pid == 0)
+	// {
+	// 	close(fd[0]);
+	// 	char x[] = "Writing from child";
+	// 	write(fd[1], &x, 19);
+	// 	close(fd[1]);
+	// }
+	// else
+	// {
+	// 	char y[19];
+	// 	close(fd[1]);
+	// 	read(fd[0], &y, 19);
+	// 	printf("In parent, writing the following message coming from child: \"%s\".\n", y);
+	// 	close(fd[0]);
+	// 	wait(NULL);
+	// }
+
+
+
+	int fd_infile = open("file1.txt", O_RDONLY); // TRYOUT 5
+	int fd_outfile = open("file2.txt", O_WRONLY);
+
+	int fd[2];
+	int fd_infile_dup = dup2(fd_infile, fd[1]);
+	int fd_outfile_dup = dup2(fd_outfile, fd[0]);
+	pipe(fd);
+	int pid = fork();
+
+	if (pid == 0)
+	{
+		close(fd[0]);
+		close(fd_outfile);
+
+		int buffer_size = 10;
+		char buffer[buffer_size];
+		int bytes_read = 1;
+
+		while (bytes_read != 0)
+		{
+			bytes_read = read(fd_infile, &buffer, buffer_size);
+			write(fd[1], &buffer, bytes_read);
+		}
+
+		close(fd[1]);
+		close(fd_infile);
+	}
+	else
+	{
+		close(fd[1]);
+		close(fd_infile);
+
+		int buffer_size = 10;
+		char buffer[buffer_size];
+		int bytes_read = 1;
+
+		while (bytes_read != 0)
+		{
+			bytes_read = read(fd[0], buffer, buffer_size);
+			write(fd_outfile, &buffer, bytes_read);
+		}
+
+		close(fd[0]);
+		close(fd_outfile);
+	}
+
+
+
+
+
+}
 
