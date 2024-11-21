b41a0c3a5c30f2bb7d996b6ff4f16a18ba597531
lagilogi <wouter_s@hotmail.nl>
2024-11-21 17:59:48 +0100

    Added toupper to make lowercase into uppercase chars, and finished exercise

--- 4-CPP_00/ex00/megaphone.cpp
+++ 4-CPP_00/ex00/megaphone.cpp
@@ -6,7 +6,11 @@ int	main(int argc, char **argv)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
 		for (int i = 1; argv[i] != NULL; i++)
+		{
+			for (int o = 0; argv[i][o] != '\0'; o++)
+				argv[i][o] = std::toupper(argv[i][o]);
 			std::cout << argv[i];
+		}
 	std::cout << '\n';
 	return 0;
 }
