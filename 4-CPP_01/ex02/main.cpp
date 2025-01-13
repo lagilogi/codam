069af1afd6c405632f7c8f2ee97e04189df54d4f
lagilogi <wouter_s@hotmail.nl>
2025-01-13 13:58:44 +0100

    removed unnecessary newline character before std::endl

--- 4-CPP_01/ex02/main.cpp
+++ 4-CPP_01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: wsonepou <wsonepou@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/12/17 14:48:19 by wsonepou      #+#    #+#                 */
-/*   Updated: 2024/12/17 17:30:54 by wsonepou      ########   odam.nl         */
+/*   Updated: 2025/01/13 13:58:01 by wsonepou      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,14 @@ int	main()
 	// addresses
 	std::cout 	<< "\nThe memory address of the string variable:   " << &str << "\n"
 				<< "The memory address held by stringPTR:        " << strPTR << "\n"
-				<< "The memory address held by stringREF:        " << &strREF << "\n" << std::endl;
+				<< "The memory address held by stringREF:        " << &strREF << std::endl;
 
 	
 
 	// values
 	std::cout 	<< "The value of the string variable:    " << str << "\n"
 				<< "The value pointed to by stringPTR:   " << *strPTR << "\n"
-				<< "The value pointed to by stringREF:   " << strREF << "\n" << std::endl;
+				<< "The value pointed to by stringREF:   " << strREF << std::endl;
 
 	return 0;
 }
