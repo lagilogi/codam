fa2b45ca7e06ca3eab08afc42a19632cc92880cd
lagilogi <wouter_s@hotmail.nl>
2025-07-14 11:21:33 +0200

    Added type checking

--- 5-CPP_08/ex00/easyfind.hpp
+++ 5-CPP_08/ex00/easyfind.hpp
@@ -12,7 +12,7 @@ template<typename T>
 typename T::iterator	easyfind(T data, int num)
 {
 	if (typeid(typename T::value_type) != typeid(int))
-		throw std::runtime_error("Invalid datatype in container!")
+		throw std::runtime_error("Invalid datatype in container!");
 	auto it = std::find(data.begin(), data.end(), num);
 	if (*it == num)
 		return (it);
